db5f6409f791818d0a2efacdfc4f28c85167ea75
balajiekk <59923131+balajiekk@users.noreply.github.com>
2026-07-19 21:11:49 +0530

    Update main.tsx
    Routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-pilot">
       <App />
     </BrowserRouter>
   </React.StrictMode>
