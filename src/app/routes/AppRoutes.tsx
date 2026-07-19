af4e880aa9b6f8e17a4f46903bc1913852d3628f
balajiekk <59923131+balajiekk@users.noreply.github.com>
2026-07-19 21:19:27 +0530

    Add Explore route

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import BalaStocksPage from "../../features/bala-stocks/BalaStocksPage";
 import Dashboard from "../../features/dashboard/Dashboard";
+import ExplorePage from "../../features/explore/ExplorePage";
 import FAndOPage from "../../features/f-and-o/FAndOPage";
 import FundsPage from "../../features/funds/FundsPage";
 import MyBalaPage from "../../features/my-bala/MyBalaPage";
@@ -16,6 +17,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={withLayout(<Dashboard />)} />
+      <Route path="/explore" element={withLayout(<ExplorePage />)} />
       <Route path="/my-bala" element={withLayout(<MyBalaPage />)} />
       <Route path="/bala-stocks" element={withLayout(<BalaStocksPage />)} />
       <Route path="/f-and-o" element={withLayout(<FAndOPage />)} />
